287d0dcc50eb4b28a4037aa893bde2d5313e2adc
Danilson Sanches <dsanches06@outlook.com>
2020-04-21 08:14:11 +0100

    Hello, World! Criando primeiro projeto e o primeiro Componente

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Curso-Angular2
 Angular (v2+) do básico ao avançado
+
+
+#### Criar novo projeto com angular-cli
+ng new nomeDoProjeto
+
+#### Gerar novo component 
+ng g c meu-primeiro2
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro-component',
+  template: ` <p>Meu primeiro componente com Angular 2</p> `,
+})
+
+export class MeuPrimeiroComponent {}
